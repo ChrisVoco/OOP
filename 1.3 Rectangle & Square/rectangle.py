b154b81dc10a5f4d88d35b526b460b30d997cfe9
Chris-Marcus Valge <chris-marcus.valge@voco.ee>
2026-03-16 11:20:02 +0200

    Add files via upload

---
+++ 1.3 Rectangle & Square/rectangle.py
@@ -0,0 +1,30 @@
+from shape import *
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return f"Width: {self.width}, Height: {self.height}"
